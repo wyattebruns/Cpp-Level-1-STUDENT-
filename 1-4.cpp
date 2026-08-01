12bd938c0efe0ff0d85de7d1c0dcda05984e3632
wyattebruns <wyattebruns@gmail.com>
2026-08-01 13:23:31 -0700

    Create 1-4.cpp

---
+++ 1-4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string repeating;
+
+    while (true)
+    {
+        cout << "Type something in!!" << "\n";
+        cin >> repeating;
+
+        if (repeating == "stop")
+        {
+            break;
+        }
+
+       
+ cout << repeating << "\n";
+    }
+
+    cout << "Goodbye!" << "\n";
+    return 0;
+}
+
+
+
+
+https://onecompiler.com/cpp#draft-zvr6
